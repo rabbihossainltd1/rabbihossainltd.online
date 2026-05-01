91887c10119667143b70cc0c912b1a14b147c7fa
rabbihossainltd1 <rabbixofficial@gmail.com>
2026-05-01 06:51:40 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -155,7 +155,7 @@
           }
         });
       },
-      { threshold: 0.1, rootMargin: '0px 0px -30px 0px' }
+      { threshold: 0, rootMargin: '0px 0px 80px 0px' }
     );
     reveals.forEach(el => observer.observe(el));
   }
